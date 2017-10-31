41678e410d2e9eb1ce1b82241cc3fc267b233396
V3Slayer <v3slayer@gmail.com>
2017-10-31 18:18:13 -0400

    Final Commit

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MvcMovie.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "CSIS 4135 - Fall 2017";
 
             return View();
         }
