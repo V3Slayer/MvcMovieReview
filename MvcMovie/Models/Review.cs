8cd184ffe47e22c9c1e05d321cb790c91a2a02b5
V3Slayer <v3slayer@gmail.com>
2017-11-09 10:25:31 -0500

    Starting Commit

---
+++ MvcMovie/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(10)]
+        [Required]
+        public string MovieReview { get; set; }
+    }
+}
